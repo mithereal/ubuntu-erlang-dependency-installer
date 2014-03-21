35a52ab8e1571bcbfd90e91618cd79a843db115f
Jason <mithereal@gmail.com>
2014-03-20 22:10:07 -0700

    initial commit

---
+++ install-erlang-deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash   
+sudo apt-get -y install fop 
+sudo apt-get -y install libncurses5-dev
+sudo apt-get -y install openjdk-6-jdk
+sudo apt-get -y install unixodbc-dev
+sudo apt-get -y install g++
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install libwxbase2.8
+sudo apt-get -y install libwxgtk2.8-dev
+sudo apt-get -y install libqt4-opengl-dev
+sudo apt-get -y install libgtk2.0-dev
+sudo apt-get -y install xsltproc
